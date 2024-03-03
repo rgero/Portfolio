17b39d8567c1ea48644125f18d150357e5e9b378
Roy Gero <roy.gero.ii@gmail.com>
2024-03-02 20:03:45 -0500

    Adding a giant amount of data to make sure that parses correctly.

--- portfolio/src/components/projects/ProjectDescription.tsx
+++ portfolio/src/components/projects/ProjectDescription.tsx
@@ -15,12 +15,12 @@ const ProjectDescription: React.FC<Props> = ({project}) => {
       <Typography variant="h5" component="h5">{project.name}</Typography>
       <hr/>
       <Grid container justifyContent="space-between" spacing={2} wrap="wrap">
-        <Grid item>
-          <Typography sx={{paddingBottom: "2rem"}}>{project.description}</Typography>
+        <Grid item xs={12} sm={(project.website || project.repo) ? 10 : 12}>
+          <Typography sx={{paddingBottom: "2rem", whiteSpace: "pre-line"}}>{project.description}</Typography>
         </Grid>
 
         {(project.website || project.repo) && (
-          <Grid item>
+          <Grid item xs={12} sm={2}>
             <Box sx={{ p: 2, border: '1px solid grey', borderRadius: 2}}>
               <Typography variant="h6" component="h6">Links</Typography>
               <List dense={true}>
--- portfolio/src/data/dummyProject.ts
+++ portfolio/src/data/dummyProject.ts
@@ -69,7 +69,7 @@ export const ProjectList: Project[] = [
     id: 3,
     name: "Empty Project",
     repo: "",
-    website: "",
+    website: "http://google.com",
     media: [
       { 
         title: "Logo for the Ling Hotel",
@@ -109,6 +109,14 @@ export const ProjectList: Project[] = [
       },
     ],
     tags: ['react', 'course'],
-    description: "This was the final project for a React course I took to expand my knowledge on React."
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu feugiat pretium nibh ipsum consequat nisl vel pretium lectus. Consectetur adipiscing elit ut aliquam purus sit amet luctus. Posuere sollicitudin aliquam ultrices sagittis orci a. Massa massa ultricies mi quis hendrerit. Sed ullamcorper morbi tincidunt ornare massa eget egestas. Egestas maecenas pharetra convallis posuere morbi leo. Est sit amet facilisis magna. Risus sed vulputate odio ut enim. Orci a scelerisque purus semper eget duis at tellus at. Vitae proin sagittis nisl rhoncus mattis rhoncus urna neque viverra. In egestas erat imperdiet sed euismod nisi porta. Est ullamcorper eget nulla facilisi.
+
+    Eget nunc scelerisque viverra mauris in. Tempus imperdiet nulla malesuada pellentesque elit eget. Faucibus in ornare quam viverra orci sagittis eu volutpat odio. Nec feugiat nisl pretium fusce id velit ut tortor pretium. Id consectetur purus ut faucibus pulvinar elementum integer. Nec feugiat in fermentum posuere urna. Sapien et ligula ullamcorper malesuada proin libero. Et leo duis ut diam quam. Risus pretium quam vulputate dignissim suspendisse in. Nascetur ridiculus mus mauris vitae ultricies leo. Bibendum at varius vel pharetra vel turpis. Sed risus ultricies tristique nulla aliquet enim tortor at. Non tellus orci ac auctor augue. Urna duis convallis convallis tellus id interdum velit. Accumsan lacus vel facilisis volutpat est velit egestas dui id. Praesent elementum facilisis leo vel fringilla.
+    
+    Leo in vitae turpis massa sed. Faucibus purus in massa tempor nec feugiat nisl. Risus viverra adipiscing at in. Lacus sed turpis tincidunt id aliquet risus feugiat in. Etiam dignissim diam quis enim lobortis scelerisque fermentum dui faucibus. Quis enim lobortis scelerisque fermentum dui faucibus. Commodo elit at imperdiet dui accumsan. Platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras. Consequat semper viverra nam libero justo laoreet. Amet massa vitae tortor condimentum lacinia. Mauris cursus mattis molestie a iaculis at. Imperdiet dui accumsan sit amet nulla facilisi morbi tempus. Orci ac auctor augue mauris. Ut tellus elementum sagittis vitae et leo duis. At consectetur lorem donec massa sapien. Condimentum mattis pellentesque id nibh tortor id. Eu mi bibendum neque egestas congue quisque egestas diam. Ut diam quam nulla porttitor massa. Posuere sollicitudin aliquam ultrices sagittis.
+    
+    Magna eget est lorem ipsum dolor sit amet. Scelerisque mauris pellentesque pulvinar pellentesque habitant morbi tristique senectus et. Mattis vulputate enim nulla aliquet porttitor. Viverra nam libero justo laoreet sit amet cursus sit. Elementum tempus egestas sed sed risus pretium quam. Diam phasellus vestibulum lorem sed risus ultricies tristique nulla. Urna cursus eget nunc scelerisque viverra mauris in aliquam sem. Volutpat lacus laoreet non curabitur gravida arcu ac. Porttitor eget dolor morbi non arcu risus. Sit amet dictum sit amet justo donec. Justo donec enim diam vulputate ut pharetra sit amet. Neque viverra justo nec ultrices dui sapien eget mi proin. Cras fermentum odio eu feugiat pretium nibh ipsum consequat nisl. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Luctus venenatis lectus magna fringilla urna porttitor rhoncus. Risus viverra adipiscing at in tellus integer feugiat scelerisque. Consequat id porta nibh venenatis cras sed felis.
+    
+    Id aliquet risus feugiat in. Cras semper auctor neque vitae tempus. Viverra ipsum nunc aliquet bibendum enim facilisis. Consequat ac felis donec et odio. Hendrerit gravida rutrum quisque non tellus orci ac auctor. Tincidunt ornare massa eget egestas purus viverra. Nec sagittis aliquam malesuada bibendum arcu vitae. Volutpat sed cras ornare arcu dui vivamus. Sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper eget. Odio facilisis mauris sit amet massa vitae tortor. Dictumst vestibulum rhoncus est pellentesque. Maecenas pharetra convallis posuere morbi. Diam vel quam elementum pulvinar etiam. Duis ut diam quam nulla porttitor. Sed euismod nisi porta lorem mollis aliquam ut porttitor. At lectus urna duis convallis convallis tellus id interdum velit. Bibendum at varius vel pharetra vel turpis nunc. Integer feugiat scelerisque varius morbi enim nunc faucibus a. Ut enim blandit volutpat maecenas volutpat blandit aliquam. Varius sit amet mattis vulputate enim nulla aliquet.`
   },
 ]
