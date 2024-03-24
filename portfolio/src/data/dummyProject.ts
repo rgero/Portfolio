08cd82421cea8e4b1b1d4b501afc22118a9e0936
Roy Gero <roy.gero.ii@gmail.com>
2024-03-24 13:37:15 -0400

    Adding dummy project data.

--- portfolio/src/data/dummyProject.ts
+++ portfolio/src/data/dummyProject.ts
@@ -66,7 +66,53 @@ export const ProjectList: Project[] = [
     description: "This was the final project for a React course I took to expand my knowledge on React."
   },
   {
-    id: 3,
+    id: 4,
+    name: "Fajita",
+    website: "",
+    repo: "https://github.com/rgero/Fajita",
+    media: [
+      { 
+        title: "Logo for the Ling Hotel",
+        type: 'image',
+        url: 'images/linghotel/logo-dark.png',
+        description: "The Logo for Ling Hotel"
+      },
+      { 
+        title: "LTT for Video Test",
+        type: 'video',
+        url: 'LfpXMuMvcWQ',
+        description: "This is a dumb video pretending to be for the project"
+      },
+      { 
+        title: "Logo for the Ling Hotel",
+        type: 'image',
+        url: 'images/linghotel/logo-dark.png',
+        description: "The Logo for Ling Hotel"
+      },
+      { 
+        title: "LTT for Video Test",
+        type: 'video',
+        url: 'LfpXMuMvcWQ',
+        description: "This is a dumb video pretending to be for the project"
+      },
+      { 
+        title: "Logo for the Ling Hotel",
+        type: 'image',
+        url: 'images/linghotel/logo-dark.png',
+        description: "The Logo for Ling Hotel"
+      },
+      { 
+        title: "LTT for Video Test",
+        type: 'video',
+        url: 'LfpXMuMvcWQ',
+        description: "This is a dumb video pretending to be for the project"
+      },
+    ],
+    tags: ['react', 'course'],
+    description: "This was the final project for a React course I took to expand my knowledge on React."
+  },
+  {
+    id: 5,
     name: "Empty Project",
     repo: "",
     website: "http://google.com",
