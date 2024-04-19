495f2a9f5d2eb5e22ae667cd0cef33ef3b2462fe
Roy Gero <roy.gero.ii@gmail.com>
2024-04-19 16:55:56 -0400

    Setting up React Query

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,29 +1,42 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import AboutPage from './pages/AboutPage';
 import AppLayout from './ui/AppLayout';
 import { DarkModeProvider } from './context/DarkModeContext';
 import ProjectListPage from './pages/ProjectListPage';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const timeInSeconds = 15;
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: timeInSeconds * 1000
+    }
+  }
+})
 
 const App = () => {
 
   return (
     <DarkModeProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route element={
-            <AppLayout/>
-          }>
-            <Route index element={<ProjectListPage/>}/>
-            <Route path="/projects" element={<ProjectListPage/>}/>
-            <Route path='/about' element={<AboutPage/>} />
-            <Route path="/resume" element={<ProjectListPage/>}/>
-          </Route>
-        </Routes>
-      </BrowserRouter>
-
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <BrowserRouter>
+          <Routes>
+            <Route element={
+              <AppLayout/>
+            }>
+              <Route index element={<ProjectListPage/>}/>
+              <Route path="/projects" element={<ProjectListPage/>}/>
+              <Route path='/about' element={<AboutPage/>} />
+              <Route path="/resume" element={<ProjectListPage/>}/>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
     </DarkModeProvider>
   )
 }
