f4bacb6810f5b222176496945ad77fb0afdb5459
Roy Gero <roy.gero.ii@gmail.com>
2024-05-04 15:48:33 -0400

    Cleaning up a console report.

--- portfolio/src/services/apiProjects.ts
+++ portfolio/src/services/apiProjects.ts
@@ -5,8 +5,6 @@ const baseImageURL = `${import.meta.env.VITE_SUPABASE_BUCKET_URL}/project_images
 export const getProjects = async (sortBy : { direction: string, field: string }) => {
   let query = supabase.from("projects").select("*");
 
-  console.log(sortBy);
-
   if (sortBy)
   {
     query = query.order(sortBy.field, {ascending: sortBy.direction === "asc"})
