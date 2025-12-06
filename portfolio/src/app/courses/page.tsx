4d49d43f7c38440328b6e33f720c050fbc1cab75
Roy Gero <roy.gero.ii@gmail.com>
2025-12-05 21:14:27 -0500

    Adding the Course page stub.

--- portfolio/src/app/courses/page.tsx
+++ portfolio/src/app/courses/page.tsx
@@ -1,11 +1,23 @@
-const page = () => {
-  return (
-    <div>
-      <h1>
-        Courses
-      </h1>
-    </div>
-  )
-}
+import { Course } from '../interfaces/Course';
+import {getCourses} from '../../services/apiCourses'
 
-export default page
+export default async function CoursesPage() {
+  const courses = await getCourses();
+
+  return (
+    <main className="p-6">
+      <h1 className="text-3xl font-bold mb-4">Courses</h1>
+
+      <ul className="space-y-2">
+        {courses.map((course: Course) => (
+          <li
+            key={course.id}
+            className="border p-3 rounded-lg"
+          >
+            <h2 className="text-xl font-semibold">{course.name}</h2>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+}
