553760ba5b75c1f95fd24896c8caf6dbf78c334e
Roy Gero <roy.gero.ii@gmail.com>
2025-12-05 19:46:25 -0500

    Adding the stub for the projects list.

--- portfolio/src/app/projects/page.tsx
+++ portfolio/src/app/projects/page.tsx
@@ -1,11 +1,23 @@
-const page = () => {
-  return (
-    <main>
-      <h1>
-        Projects
-      </h1>
-    </main>
-  )
-}
+import { Project } from '../interfaces/Project';
+import {getProjects} from '../../services/apiProjects'
 
-export default page
+export default async function ProjectsPage() {
+  const projects = await getProjects();
+
+  return (
+    <main className="p-6">
+      <h1 className="text-3xl font-bold mb-4">Projects</h1>
+
+      <ul className="space-y-2">
+        {projects.map((project: Project) => (
+          <li
+            key={project.id}
+            className="border p-3 rounded-lg"
+          >
+            <h2 className="text-xl font-semibold">{project.name}</h2>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+}
