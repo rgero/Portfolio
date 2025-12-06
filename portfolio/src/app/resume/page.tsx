3336e5d7b578e89dcd6f824850c708176ae5bf62
Roy Gero <roy.gero.ii@gmail.com>
2025-12-05 20:27:21 -0500

    Adding a stub for the resume page.

--- portfolio/src/app/resume/page.tsx
+++ portfolio/src/app/resume/page.tsx
@@ -1,11 +1,17 @@
-const page = () => {
-  return (
-    <main>
-      <h1>
-        Resume
-      </h1>
-    </main>
-  )
-}
+export default function ResumePage() {
+  const SUPABASE_BUCKET_URL = process.env.VITE_SUPABASE_BUCKET_URL;
+  const pdfUrl = `https://docs.google.com/gview?embedded=true&url=${SUPABASE_BUCKET_URL}/files/GeroResume.pdf`;
 
-export default page
+  return (
+    <div className="flex flex-col items-center p-8">
+      <h1 className="text-3xl font-bold mb-6">Resume</h1>
+
+      <embed
+        src={pdfUrl}
+        type="application/pdf"
+        className="w-full rounded-xl"
+        style={{ height: "80vh" }}
+      />
+    </div>
+  );
+}
